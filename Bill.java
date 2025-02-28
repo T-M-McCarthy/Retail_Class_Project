8bc276fbfbf9f7438d35fb616f5a003373c5b9f5
Thomas McCarthy <120833756+T-M-McCarthy@users.noreply.github.com>
2025-02-27 21:15:11 -0500

    Add files via upload

---
+++ Bill.java
@@ -0,0 +1,154 @@
+package TheFinalProject;
+
+
+
+import java.util.*;
+
+
+/**
+ *
+ * @author phferris
+ */
+public class Bill{
+    double total;
+    private  ArrayList<String> shoppingItems = new ArrayList<>();
+    private HashMap<String, Double> values = new HashMap<>();
+
+    String[] items;
+    Integer[] quantity;
+
+    /**
+     *
+     */
+    public Bill(){
+    }
+
+    /**
+     *
+     * @param shoppingItems this is needed to form one of the arrays to be fed into the total and payment methods. It stores the different Items
+     * @param itemQuantity  this is needed to form one of the arrays to be fed into the total and payment methods. It stores the amount of each item
+     */
+    public Bill(ArrayList<String> shoppingItems, ArrayList<Integer> itemQuantity){
+        this.shoppingItems = shoppingItems;
+        items = new String[shoppingItems.size()];
+        quantity = new Integer[itemQuantity.size()];
+        items = shoppingItems.toArray(items);
+        quantity = itemQuantity.toArray(quantity);
+
+        }
+
+
+    /**
+     *
+     * @param customerFound grabs the variable that denotes if the user is a card holding member
+     * @return returns the total cost of the sale
+     */
+
+    public double total(boolean customerFound) {
+        // two hash maps are used to quickly and efficiently sort through all data
+        HashMap<String, Double> values = new HashMap<>();
+        values.put("cereal", 2.00);
+        values.put("shampoo", 3.00);
+        values.put("canned vegetables", 3.50);
+        HashMap<String, Integer> totalMap = new HashMap<>();
+        for (int i = 0; i < shoppingItems.size(); i++) {
+            totalMap.put(items[i], quantity[i]);
+        }
+        double total = 0.0;
+            for (String item : totalMap.keySet()) {
+                int quantity = totalMap.get(item);
+                if (values.containsKey(item)) {
+                        if (customerFound){
+                            switch (item) {
+                                case "cereal" : {
+                                    // The following algorithm is adapted from the CerealBox class created by Seska Linn
+                                    int setsOfFour = quantity / 4;
+                                    int remainingCereals = quantity % 4;
+                                    double discountedPrice = values.get(item) * 0.5;
+                                    double regularPrice = values.get(item);
+                                    total += setsOfFour * (discountedPrice * 4) + remainingCereals * regularPrice;
+                                    break;
+                                }
+                                case "shampoo" :{
+                                    // The following algorithm is adapted from the Shampoo class created by Wesley Freeman
+                                    int setsOfTwo = quantity / 2;
+                                    int remainingShampoos = quantity % 2;
+                                    double shampooPrice = values.get(item);
+                                    total += setsOfTwo * 3.00 + remainingShampoos * shampooPrice;
+                                    break;
+                                }
+                                case "canned vegetables" : {
+                                    // The following algorithm is adapted from the Canned Vegetables class created by Joshua Garcia
+                                    int setsOfThree = quantity / 3;
+                                    int remainingSoups = quantity % 3;
+                                    double soupPrice = values.get(item);
+                                    double specialOfferPrice = setsOfThree * 10.50;
+                                    double remainingItemsPrice = remainingSoups * soupPrice;
+                                    total += specialOfferPrice + remainingItemsPrice;
+                                    break;
+                                }
+                                default : total +=  quantity;
+                                break;
+                            }
+                } else {
+
+                            switch (item) {
+                                case "cereal" : total += quantity * 2.00;
+                                break;
+                                case "shampoo" : total += quantity * 3.00;
+                                break;
+                                case "canned vegetables" : total += quantity * 3.50;
+                                break;
+                                default : total += quantity;
+                                break;
+                            }
+                        }
+
+                    } else {
+                    total += quantity;
+                    }
+                }
+                 System.out.println(totalMap);
+            return total;
+            }
+
+
+    /**
+     *
+     * @param total This is used to determine how much the system needs to be paid. the parameter due is based on total- cash supplied to store.
+     */
+    public void payment(double total) {
+        System.out.println("Total: " + total);
+        System.out.println("please insert cash");
+        System.out.println("How much cash was given?");
+        Scanner bob = new Scanner(System.in);
+        double cash = bob.nextDouble();
+        if (total - cash < 0 ){
+            System.out.println("Thank you for shopping at our Final Project");
+            System.out.printf("Your change is: $%.2f%n",  Math.abs(total- cash));
+        }
+        else if (total- cash > 0) {
+            System.out.println("Insufficient funds.");
+            double due = total- cash;
+            System.out.println("");
+            System.out.printf("You still owe: $%.2f%n", Math.abs(due));
+            while (due > 0 ){
+                System.out.println("please insert cash");
+                System.out.println("How much cash was given?");
+                due = (due - bob.nextDouble()) ;
+            }
+            if (due < 0){
+                System.out.println("Thank you for shopping at our Final Project");
+                System.out.printf("Your change is: $%.2f%n", Math.abs(due));
+            }
+        }
+        else{
+            System.out.println("Thank you for shopping at our Final Project");
+            System.out.println("Have a nice day!");
+        }
+    }
+}
+
+
+
+
---
+++ Retail.java
@@ -0,0 +1,252 @@
+package TheFinalProject;
+
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+/*
+The Retail class was created by Thomas McCarthy
+Edited and Revised for integration and comprehension by Paul Ferris
+ */
+public class Retail {
+
+    // customer Input is a class intended to make the options menu less cluttered and more accessible
+    public static void customerInput() {
+        System.out.println("1. Checkout");
+        System.out.println("2. Create New Customer");
+        System.out.println("3. Update Existing Customer");
+        System.out.println("4. Search Customer");
+        System.out.println("5. Delete Customer");
+        System.out.println("6. Exit");
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Grocery> customerInfo = new ArrayList<>();
+        boolean existingCustomer = false;
+        boolean customerFound = false;
+        int option;
+        ArrayList<String> shoppingItems = new ArrayList<>();
+        ArrayList<Integer> itemQuantity = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Welcome to <Generic Supermarket>");
+        System.out.println("1. Checkout with a membership");
+        System.out.println("2. Checkout without a membership");
+        System.out.println("3. Exit");
+        option = in.nextInt();
+        /* This switch was implemented to give the user a chance to opt out of coupons and deals. This design choice
+        * was made due to real world applications. People opt for their information to not be stored and would be
+        * willing to pay more for that.
+         */
+        switch (option) {
+
+            case 1 : {
+                Grocery customer = new Grocery();
+                
+                do {
+                    customerInput();
+                    System.out.println("Enter your option: ");
+                    option = in.nextInt();
+                    // The following switch is designed to allow for the user to use their account/loyalty card to receive
+                    // discounts OR to sign up as a member.
+                    switch (option) {
+                        case 1 : {
+                            // Case one is designed to be the go-to option for checking out at a store. It checks if
+                            // you are a member and will not let you checkout with the discounts unless you are a member
+                            System.out.println("Enter customer's first name to search: ");
+                            String findFirstName = in.next();
+                            System.out.println("Enter customer's last name to search: ");
+                            String findLastName = in.next();
+
+                            for (Grocery findCustomer : customerInfo) {
+                                if (findCustomer.getCustomerFirstName().equalsIgnoreCase(findFirstName) && findCustomer.getCustomerLastName().equalsIgnoreCase(findLastName)) {
+                                    System.out.println("Customer name: " + findCustomer.getCustomerFirstName() + " " + findCustomer.getCustomerLastName());
+                                    customerFound = true;
+                                    break;
+                                }
+                            }
+                            if (!customerFound) {
+                                System.out.println("Customer not found");
+                                break;
+                            }
+
+                            System.out.println("For every 20 dollars, you get 5 points AND when you reach 500 points, you will get a 10 dollar discount!");
+
+                            for (int i = 0; i < 6; i++) {
+                                /* I edited the logic of this code to include a switch so people could exit prior to
+                                     * the end of the for loop. A while loop would be much better in applications
+                                     * while (in.next().toLowerCase() != "exit" || in.next().toLowerCase() != "done" || in.next().toLowerCase() != "q")
+                                     * I decided to keep in the for loop to allow for quick testing of all 3 products and not worrying about returned null values if
+                                     * the algorithm ended in an odd spot
+                                 */
+
+                                // This is the basis of the storage of values in the program
+                                in.nextLine();
+                                System.out.println("Please Enter the Item You Wish to Purchase: ");
+                                String items = in.nextLine();
+                                System.out.println("Please Enter the amount of this item: ");
+                                int quantity = in.nextInt();
+                                shoppingItems.add(items.toLowerCase());
+                                itemQuantity.add(quantity);
+
+                            }
+
+                            customer.setShoppingItems(shoppingItems);
+                            customer.setItemQuantity(itemQuantity);
+
+                            for (Grocery name : customerInfo) {
+                                System.out.println(name);
+                            }
+                            // A bill object is created to send the arrays storing the items and the quantity of items
+                            // to the object so the class can be used.
+                            Bill bob = new Bill(shoppingItems, itemQuantity);
+
+                            // The following operations are performed using algorithms from the classes created by Seska Linn, Wesley Freeman, and Joshua Garcia.
+                            bob.payment(bob.total(customerFound) - customer.getDiscountAmount());
+
+                            // The following operations utilize the loyalty card class created by Joshua Collins
+                            customer.addPoints(bob.total(customerFound));
+                            System.out.println("You have " + customer.getPoints() + " loyalty points.");
+                            break;
+                        }
+
+                        case 2 : {
+                            // If the User is new, They can create their own membership
+                            System.out.println("Add Customer");
+                            System.out.println("Please Enter Customer First Name: ");
+                            String firstName = in.next();
+                            System.out.println("Please Enter Customer Second Name: ");
+                            String lastName = in.next();
+                            // The following code is used to ensure no redundancies exist in the lists
+                            for (Grocery findCustomer : customerInfo) {
+                                if (findCustomer.getCustomerFirstName().equalsIgnoreCase(firstName) && findCustomer.getCustomerLastName().equalsIgnoreCase(lastName)) {
+                                    System.out.println("This Individual is Already an Existing Customer");
+                                    System.out.println("Do you want to continue shopping? Y or N");
+                                    String answer = in.next();
+                                    existingCustomer = !answer.equalsIgnoreCase("Y");
+                                }
+                            }
+                            if (!existingCustomer) {
+                                System.out.println("Welcome to the <Generic Superstore> family!");
+                                customer.setCustomerFirstName(firstName);
+                                customer.setCustomerLastName(lastName);
+                                customer.makeCard();
+                                customerInfo.add(customer);
+                            }
+                            break;
+                        }
+
+                        case 3 : {
+                            // Sometimes people make typos when making a profile, or some life event happened for their
+                            // name to change. This case allows Customers to change their names to match their preferences
+                            System.out.println("Update Customer");
+                            System.out.println("Please Enter the Customer's First Name You Wish to Update: ");
+                            String searchFirstName = in.next();
+                            System.out.println("Please Enter the Customer's Last Name You Wish to Update: ");
+                            String searchLastName = in.next();
+                            for (Grocery updateCustomer : customerInfo) {
+                                if (updateCustomer.getCustomerFirstName().equalsIgnoreCase(searchFirstName) && updateCustomer.getCustomerLastName().equalsIgnoreCase(searchLastName)) {
+                                    System.out.println("Please Enter New Customer's First Name: ");
+                                    String newFirstName = in.next();
+                                    updateCustomer.setCustomerFirstName(newFirstName);
+                                    System.out.println("Please Enter New Customer's Last Name: ");
+                                    String newLastName = in.next();
+                                    updateCustomer.setCustomerLastName(newLastName);
+                                    break;
+                                } else {
+                                    System.out.println("Customer not found. Please Try Again.");
+                                }
+                            }
+                            break;
+                        }
+                        case 4 : {
+                            // Sometimes people need to see if they are a member or not before they make a decision on
+                            // buying from a place. This class allows the user to check if they are in the system
+                            System.out.println("Search Customer");
+                            System.out.println("Enter customer's first name to search: ");
+                            String findFirstName = in.next();
+                            System.out.println("Enter customer's last name to search: ");
+                            String findLastName = in.next();
+
+                            for (Grocery findCustomer : customerInfo) {
+                                if (findCustomer.getCustomerFirstName().equalsIgnoreCase(findFirstName) && findCustomer.getCustomerLastName().equalsIgnoreCase(findLastName)) {
+                                    System.out.println("Customer name: " + findCustomer.getCustomerFirstName() + " " + findCustomer.getCustomerLastName());
+                                    customerFound = true;
+                                    break;
+                                }
+                            }
+                            if (!customerFound) {
+                                System.out.println("Customer not found");
+                            }
+                            System.out.println();
+                            break;
+                        }
+                        case 5 : {
+                            // case 5 is set up to remove a customer
+                            System.out.println("Delete Customer");
+                            System.out.println("Enter Customer's First Name: ");
+                            String deleteFirstName = in.next();
+                            System.out.println("Enter Customer's Last Name: ");
+                            String deleteLastName = in.next();
+                            Grocery customerToDelete = null; // Create a reference to the customer to delete
+                            for (Grocery deleteCustomer : customerInfo) {
+                                if (deleteCustomer.getCustomerFirstName().equalsIgnoreCase(deleteFirstName) && deleteCustomer.getCustomerLastName().equalsIgnoreCase(deleteLastName)) {
+                                    System.out.println("Customer Name: " + deleteCustomer.getCustomerFirstName() + " " + deleteCustomer.getCustomerLastName());
+                                    System.out.println("Are You Sure You Want To Delete This Customer? Please Enter: Y or N ");
+                                    String confirm = in.next();
+
+                                    if (confirm.equalsIgnoreCase("Y")) {
+                                        customerToDelete = deleteCustomer; // Set the customer to delete
+                                        break;
+                                    }
+                                }
+                            }
+                            if (customerToDelete != null) {
+                                customerInfo.remove(customerToDelete); // Remove the customer from the ArrayList
+                                System.out.println("Customer has been deleted.");
+                            } else {
+                                System.out.println("Customer not found.");
+                            }
+                            break;
+                        }
+                        // Case 6 is the early, but voluntary exit in the program
+                        case 6 :
+                            System.out.println("Exit");
+                            break;
+                        default :
+                            System.out.println("Invalid choice");
+                            break;
+                    }
+
+                } while (option
+                        != 6);
+             break;   
+            }
+            case 2 : {
+                // This program is simply set up to charge the user without a discount
+                System.out.println("To apply coupons, go back and select option 1 and make a membership");
+                for (int i = 0; i < 6; i++) {
+                    in.nextLine();
+                    System.out.println("Please Enter the Item You Wish to Purchase: ");
+                    String items = in.nextLine();
+                    System.out.println("Please Enter the amount of this item: ");
+                    int quantity = in.nextInt();
+                    shoppingItems.add(items);
+                    itemQuantity.add(quantity);
+                    Bill bob = new Bill(shoppingItems, itemQuantity);
+                    bob.payment(bob.total(customerFound));
+                }
+                break;
+            }
+
+            case 3 :
+                System.out.println("Exiting");
+                break;
+
+            default :
+                System.out.println("Have a nice day!");
+                break;
+        }
+    }
+}
